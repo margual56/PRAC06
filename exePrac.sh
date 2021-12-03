abe802b24fdb761bba493c0776233fb9899383f7
Gutierrez Alonso Marcos <PCP58441161R@di122.edv.uniovi.es>
2021-12-03 09:32:09 +0100

    Added threads per block

--- exePrac.sh
+++ exePrac.sh
@@ -6,21 +6,18 @@
 #sizes=(1 10 101 500 1001)
 sizes=(10)
 #block_sizes=(1 1 1 1 1)
-block_sizes=(1)
+thr_block=(1) # Threads per block
 
 for img in /opt/PracticasPCP2122/Imagenes/* 
 do
 	printf "\n\n$img\nveces;secuencial;openmp\n"
 	
+	i=0
 	for size in ${sizes[@]};
 	do
 		#echo "./PRAC04 $img "output.bmp" $size"
 
-		export OMP_NUM_THREADS=1
-		sec=$(./Filtro $img "output.bmp" $size)
-
-		unset OMP_NUM_THREADS
-		par=$(./Filtro $img "output.bmp" $size)
+		cuda=$(./Filtro $img "output.bmp" $size ${thr_block[$i]})
 
 		printf "$size;${sec};${par}\n"
 		#./profesores $img_name "outputP.bmp" $2 $3
